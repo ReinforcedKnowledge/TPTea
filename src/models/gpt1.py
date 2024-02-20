5541787f4b24ba1b844119d566ba7c617bc1ed0d
ReinforcedKnowledge <reinforced.knowledge@gmail.com>
2024-02-20 19:16:16 +0100

    import causal self attention

--- src/models/gpt1.py
+++ src/models/gpt1.py
@@ -3,13 +3,13 @@ We're trying to closely follow the descriptions provided in this paper: Radford,
 The paper also talks about training and optimization. This will be implemented later as a training scheme. More details can be found on my blog: https://reinforcedknowledge.com
 """
 
-import math
 from dataclasses import dataclass
 from typing import Callable, Optional
 
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
+
+from .causal_self_attention import CausalSelfAttention
 
 
 def gpt1_initialization(model):
@@ -34,61 +34,6 @@ class GPT1Config:
     n_head: int = 12
     n_embd: int = 768
     initialization: Optional[Callable] = gpt1_initialization
-
-
-# Causal Self-Attention
-class CausalSelfAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
-        # Combining key, query, and value in a single matrix
-        self.key_query_value = nn.Linear(config.n_embd, 3 * config.n_embd)
-        self.proj = nn.Linear(config.n_embd, config.n_embd)
-        self.dropout = nn.Dropout(0.1)
-
-        # Make the causal mask a part of the module's state through register_buffer ensures
-        self.register_buffer(
-            "mask",
-            torch.tril(torch.ones(config.context_size, config.context_size)).view(
-                1, 1, config.context_size, config.context_size
-            ),
-        )
-
-    def forward(self, x):
-        batch_size, sequence_length, embed_size = x.size()
-
-        # Project to key, query, value in a single pass
-        kqv = self.key_query_value(x)
-        k, q, v = kqv.split(embed_size, dim=-1)
-
-        # Reshape for multi-head attention
-        def reshape_to_heads(tensor):
-            return tensor.view(
-                batch_size,
-                sequence_length,
-                self.config.n_head,
-                embed_size // self.config.n_head,
-            ).transpose(1, 2)
-
-        k, q, v = map(reshape_to_heads, [k, q, v])
-
-        # Scaled dot product attention
-        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        attn = attn.masked_fill(
-            self.mask[:, :, :sequence_length, :sequence_length] == 0, float("-inf")
-        )
-        attn = F.softmax(attn, dim=-1)
-        attn = self.dropout(attn)
-
-        # Apply attention to the value and combine the heads back to the original tensor shape
-        y = (
-            (attn @ v)
-            .transpose(1, 2)
-            .contiguous()
-            .view(batch_size, sequence_length, embed_size)
-        )
-
-        return self.proj(y)
 
 
 # MLP
